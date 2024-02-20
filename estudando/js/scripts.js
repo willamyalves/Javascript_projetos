30234127d4f3f2ae7f79b620c322734eef480f83
willamyDev <willamy9100@gmail.com>
2024-02-20 10:20:58 -0300

    changed

---
+++ estudando/js/scripts.js
@@ -0,0 +1,12 @@
+let box = document.querySelector(".box");
+
+// box.addEventListener("mousedown", ()=>{
+//     console.log("Clicado");
+// })
+// box.addEventListener("mouseup", ()=>{
+//     console.log("Soltou");
+// })
+
+box.addEventListener("dblclick", ()=>{
+    console.log("Duplo clique");
+})
